3fe51c5e78b8cc1683db1ad9a2f9fa077269d3f5
ottoniel <ottonielmolina@gmail.com>
2024-06-06 23:41:13 -0600

    feat(app): cambios para implementar búsqueda de lugares para visualizar previsión meteorológica.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { LocationService } from './services/location/location.service';
 import { firstValueFrom } from 'rxjs';
-import { IpExternal } from './models/ip-external.model';
 import { SessionstorageService } from './services/storage/sessionstorage.service';
+import { Place } from './models/place.model';
+
+const TRIGGER_BUSCAR = 750;
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,9 @@ import { SessionstorageService } from './services/storage/sessionstorage.service
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  private refTimeout: any;
   title = 'web-weather';
+  public listPlaces: Array<Place> = [];
 
   constructor(
     private locationService: LocationService,
@@ -30,4 +34,32 @@ export class AppComponent {
     }
   }
 
+  name = '';
+
+  search(event: any) {
+    clearTimeout(this.refTimeout);
+    this.refTimeout = setTimeout(() => {
+      if(event.trim() === '') {
+        return;
+      }
+      this.searchingProcess(event);
+    }, TRIGGER_BUSCAR);
+  }
+
+  async searchingProcess(place: string) {
+    let list = await firstValueFrom(this.locationService.getListPlaces(place));
+    this.listPlaces = [];
+    list = list.filter((item:Place, index:number) => {
+      return list.findIndex(e => e.display_name === item.display_name) === index;
+    });
+    this.listPlaces = list;
+    this.listPlaces = [
+      ...this.listPlaces.filter(e => e.display_name.toLocaleLowerCase().includes(this.sessionStorage.countryIpAddressExternal.toLocaleLowerCase())),
+      ...this.listPlaces.filter(e => !e.display_name.toLocaleLowerCase().includes(this.sessionStorage.countryIpAddressExternal.toLocaleLowerCase()))
+    ]
+  }
+
+  public selectResult(item:Place) {
+    console.log('itemSelected', item);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './components/header/header.module';
+import { FormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
 
@@ -15,7 +16,9 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HeaderModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    HeaderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
