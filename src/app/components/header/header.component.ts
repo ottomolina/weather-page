962597616377baf0e3cda9a6afede57484805c4b
ottoniel <ottonielmolina@gmail.com>
2024-06-06 23:35:25 -0600

    feat(header): cambios en el componente header para realizar búsquedas.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Place } from 'src/app/models/place.model';
 
 @Component({
   selector: 'app-header',
@@ -6,11 +7,31 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  @Input() listPlaces: Array<Place> = [];
   @Input() disabled = false;
   @Output() onInput = new EventEmitter();
+  @Output() onSelectResult = new EventEmitter();
+
+  public showResult = false;
 
   inputEvent(e: any) {
     this.onInput.emit(e.target.value);
+  }
+
+  focusSearch() {
+    setTimeout(() => {
+      this.showResult = true;
+    }, 250);
+  }
+
+  blurSearch() {
+    setTimeout(() => {
+      this.showResult = false;
+    }, 100);
+  }
+
+  clickItemResult(itemResult: Place) {
+    this.onSelectResult.emit(itemResult);
   }
 
 }
