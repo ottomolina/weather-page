3da5da8fa100a74d796532f189273026a7056879
ottoniel <ottonielmolina@gmail.com>
2024-06-20 00:18:42 -0600

    feat(card-day-weather): se agrega component card para clima general diario.

---
+++ src/app/models/daily-weather.model.ts
@@ -0,0 +1,10 @@
+
+export class DailyWeather {
+    nameDay: string;
+    date: string;
+    precipitation_sum: number;
+    precipitation_probability_max: number;
+    weather_code: number;
+    temperature_2m_max: number;
+    temperature_2m_min: number;
+}
