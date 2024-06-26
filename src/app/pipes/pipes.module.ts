8c842ba89419e81f245b4778bef87a7e6370a468
ottoniel <ottonielmolina@gmail.com>
2024-06-25 23:13:43 -0600

    feat(pipes): se agregan pipes relativos a la dirección del viento.
    Se agrega pipe de traducción de la dirección del viento y pipe de traducción de ícono de dirección del viento.

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { WeatherCodePipe } from './weather-code.pipe';
 import { FormatNumberPipe } from './format-number.pipe';
 import { WeatherCodeIconPipe } from './weather-code-icon.pipe';
+import { WindDirectionIconPipe } from './wind-direction-icon.pipe';
+import { WindDirectionPipe } from './wind-direction.pipe';
 
 @NgModule({
     declarations: [
         WeatherCodePipe,
         FormatNumberPipe,
-        WeatherCodeIconPipe
+        WeatherCodeIconPipe,
+        WindDirectionIconPipe,
+        WindDirectionPipe
     ],
     exports: [
         WeatherCodePipe,
         FormatNumberPipe,
-        WeatherCodeIconPipe
+        WeatherCodeIconPipe,
+        WindDirectionIconPipe,
+        WindDirectionPipe
     ],
 })
 
