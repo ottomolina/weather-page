0bc07394bf65dc76db9b749793d777c38ebbf7d2
ottoniel <ottonielmolina@gmail.com>
2024-06-26 23:29:43 -0600

    fix(api-country): cambio de url de api para detección de pais por ip.
    Se actualiza modelo de datos y lógica de negocio para obtener el resultado del pais.

--- src/app/models/country-ip-address.model.ts
+++ src/app/models/country-ip-address.model.ts
@@ -1,17 +1,5 @@
 
 export class CountryIpAddress {
-    query: string;
-    status: string;
+    ip: string;
     country: string;
-    countryCode: string;
-    region: string;
-    regionName: string;
-    city: string;
-    zip: string;
-    lat: string;
-    lon: string;
-    timezone: string;
-    isp: string;
-    org: string;
-    as: string;
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     urlDetectIp: 'https://ipv4.myexternalip.com/json',
-    urlCountryIpAddress: 'http://ip-api.com/json/',
+    urlCountryIpAddress: 'https://api.country.is/',
     urlPlaces: 'https://nominatim.openstreetmap.org/search?format=json&q={place}',
     urlWeather: 'https://api.open-meteo.com/v1/forecast?current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,weather_code&hourly=temperature_2m,apparent_temperature,wind_speed_10m,wind_gusts_10m,wind_direction_10m,relative_humidity_2m,precipitation,precipitation_probability,weather_code&daily=sunrise,sunset,precipitation_probability_max,precipitation_sum,weather_code,temperature_2m_max,temperature_2m_min&timezone=auto&latitude={lat}&longitude={lon}&forecast_days=14',
 };
